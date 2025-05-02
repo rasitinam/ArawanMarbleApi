c2389e0d394c97694de88c61546436c2403ec4f8
Raşit <inamrasit@gmail.com>
2025-05-02 20:53:51 +0300

    plz

--- obj/Release/net9.0/ArawanMarbleApi.AssemblyInfo.cs
+++ obj/Release/net9.0/ArawanMarbleApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArawanMarbleApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b821c0de43fd426274416735f3f8cc072f9f49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad68503040f7a295779d4a71a2f606db80f8965")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArawanMarbleApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArawanMarbleApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
